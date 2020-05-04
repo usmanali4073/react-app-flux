bb11a302419c4ef57f8d7e05911c9c8cd96cd51f
usman ali <usmanali4073@gmail.com>
2020-05-04 16:22:09 -0400

    added header

--- src/common/Headers.jsx
+++ src/common/Headers.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
+import { NavLink } from "react-router-dom";
 
 export default class Headers extends Component {
   render() {
     return (
       <div>
         <nav>
-          <a href="/">Home</a> | <a href="/about">About</a> |{" "}
-          <a href="/courses">Courses</a>
+          <NavLink to="/" activeClassName="active">
+            Home
+          </NavLink>
+          |<NavLink to="/about">About</NavLink> |{" "}
+          <NavLink to="/courses">courses</NavLink>
         </nav>
       </div>
     );
