85f23dbae8b67ccba03d9e35db0e8b93ece60fc8
usman ali <usmanali4073@gmail.com>
2020-05-04 14:13:56 -0400

    added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,25 +1,20 @@
-import React from "react";
 import HomePage from "./HomePage";
 import About from "./About";
 import Headers from "../common/Headers";
 import CoursesPage from "./CoursesPage";
 
-function App() {
-  function getPage() {
-    const route = window.location.pathname;
-    if (route === "/about") {
-      return <About />;
-    } else if (route === "/courses") {
-      return <CoursesPage />;
-    }
-    return <HomePage />;
-  }
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
 
-  return (
-    <React.Fragment>
-      <Headers />
-      {getPage()}
-    </React.Fragment>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Headers />
+        <Route exact={true} path="/" component={HomePage} />
+        <Route path="/about" component={About} />
+        <Route path="/courses" component={CoursesPage} />
+      </React.Fragment>
+    );
+  }
 }
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import React from "react";
 import { render } from "react-dom";
 
 import App from "./components/App";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
